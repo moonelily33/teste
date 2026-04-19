b904a2cf81bec90bfa7f014904afdd5295b57e06
moonelily33 <moonelily33@gmail.com>
2026-04-20 00:18:44 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,241 @@
+const data = [
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "SMTOWN & SHOP"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "ld",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "ld",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "fm",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "fm",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "fm",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  {
+    img: "img/jeno1.jpg",
+    member: "jeno",
+    benefit: "pob",
+    era: "gbttf",
+    title: "Jeno POB"
+  },
+  {
+    img: "img/jaemin1.jpg",
+    member: "jaemin",
+    benefit: "album",
+    era: "biu",
+    title: "Jaemin Album"
+  },
+  
+];
